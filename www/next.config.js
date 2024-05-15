51c23fe23d3d9135d1a8e4584e8870e671572a41
Hector Crean <hectorcrean@gmail.com>
2024-05-15 15:37:54 +0100

    ignore type erorrs on build

--- www/next.config.js
+++ www/next.config.js
@@ -5,8 +5,14 @@ const nextConfig = {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
-      },
-
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
 }
 
 module.exports = nextConfig
