1f48244a31c121e46186a26aeeea6ec84b64580b
Hector Crean <hectorcrean@gmail.com>
2024-06-19 11:02:01 +0100

    move nav

---
+++ view/src/nav.ts
@@ -0,0 +1,21 @@
+import { router } from "@/main";
+
+import { routes } from '@/main';
+
+
+
+const page = {
+    init: () => {
+
+        routes.forEach(route => {
+            document?.querySelector(`#${route.id}`)?.addEventListener("click", () => {
+                router.redirectTo(route.path);
+            });
+        })
+
+    },
+};
+
+
+
+export default page
